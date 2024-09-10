fecdd41e326809195154f50e0d6588aaf3172909
Sayyed Zunnurain <sayyed078680@gmail.com>
2024-09-10 17:55:55 +0530

    app.js changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 //static file
-app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.use("/api", Routes);
 
